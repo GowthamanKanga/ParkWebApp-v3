6f429c5583db9cf112e33225af2dea65db8189ff
GowthamanKanga <83148850+GowthamanKanga@users.noreply.github.com>
2023-02-11 02:33:46 -0500

    Add files via upload

---
+++ src/pages/ParkMap.jsx
@@ -0,0 +1,253 @@
+// import React, { useRef, useEffect } from "react";
+// import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
+
+// const parks = [
+//   {
+//     name: "High Park",
+//     location: "Toronto, ON",
+//     id: 1,
+//     lat: 43.653208,
+//     lng: -79.463415,
+//   },
+// ];
+
+// const ParkMap = ({ google }) => {
+//   const mapRef = useRef(null);
+
+//   useEffect(() => {
+//     const map = new google.maps.Map(mapRef.current, {
+//       zoom: 12,
+//       center: { lat: 43.653208, lng: -79.463415 },
+//     });
+
+//     parks.forEach((park) => {
+//       const marker = new google.maps.Marker({
+//         position: { lat: park.lat, lng: park.lng },
+//         map,
+//         title: park.name,
+//       });
+//     });
+//   }, [google]);
+
+//   return (
+//     <div>
+//       <body className="bg-white">
+//         <nav className="fixed top-0 left-0 z-20 w-full border-b border-gray-200 bg-white py-2.5 px-6 sm:px-4">
+//           <div className="container mx-auto flex max-w-6xl flex-wrap items-center justify-between">
+//             <a href="#" className="flex items-center">
+//               <span className="self-center whitespace-nowrap text-xl font-semibold">
+//                 GBC Park & Recreation
+//               </span>
+//             </a>
+//             <div className="mt-2 sm:mt-0 sm:flex md:order-2">
+//               <button
+//                 type="button"
+//                 className="rounde mr-3 hidden border border-blue-700 py-1.5 px-6 text-center text-sm font-medium text-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 md:inline-block rounded-lg"
+//               >
+//                 Login
+//               </button>
+//               <button
+//                 type="button"
+//                 className="rounde mr-3 hidden bg-blue-700 py-1.5 px-6 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 md:mr-0 md:inline-block rounded-lg"
+//               >
+//                 Register
+//               </button>
+//               <button
+//                 data-collapse-toggle="navbar-sticky"
+//                 type="button"
+//                 className="inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden"
+//                 aria-controls="navbar-sticky"
+//                 aria-expanded="false"
+//               >
+//                 <svg
+//                   className="h-6 w-6"
+//                   aria-hidden="true"
+//                   fill="currentColor"
+//                   viewBox="0 0 20 20"
+//                   xmlns="http://www.w3.org/2000/svg"
+//                 >
+//                   <path
+//                     fill-rule="evenodd"
+//                     d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+//                     clip-rule="evenodd"
+//                   ></path>
+//                 </svg>
+//               </button>
+//             </div>
+//             <div
+//               className="hidden w-full items-center justify-between md:order-1 md:flex md:w-auto"
+//               id="navbar-sticky"
+//             >
+//               <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:text-sm md:font-medium">
+//                 <li>
+//                   <a
+//                     href="#"
+//                     className="block rounded bg-blue-700 py-2 pl-3 pr-4 text-white md:bg-transparent md:p-0 md:text-blue-700"
+//                     aria-current="page"
+//                   >
+//                     Home
+//                   </a>
+//                 </li>
+//                 <li>
+//                   <a
+//                     href="#"
+//                     className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700"
+//                   >
+//                     Park List
+//                   </a>
+//                 </li>
+//               </ul>
+//             </div>
+//           </div>
+//         </nav>
+
+//         <div className="pt-32  bg-white">
+//           <h1 className="text-center text-2xl font-bold text-gray-800">
+//             Map
+//           </h1>
+//         </div>
+
+//         <div className="flex flex-wrap items-center  overflow-x-auto overflow-y-hidden py-10 justify-center   bg-white text-gray-800">
+//           <a
+//             rel="noopener noreferrer"
+//             href="#"
+//             className="flex items-center flex-shrink-0 px-5 py-3 space-x-2text-gray-600"
+//           >
+//             <svg
+//               xmlns="http://www.w3.org/2000/svg"
+//               viewBox="0 0 24 24"
+//               fill="none"
+//               stroke="currentColor"
+//               stroke-width="2"
+//               stroke-linecap="round"
+//               stroke-linejoin="round"
+//               className="w-4 h-4"
+//             >
+//               <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+//             </svg>
+//             <span>Park Info</span>
+//           </a>
+//           <a
+//             rel="noopener noreferrer"
+//             href="#"
+//             className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 rounded-t-lg text-gray-900"
+//           >
+//             <svg
+//               xmlns="http://www.w3.org/2000/svg"
+//               viewBox="0 0 24 24"
+//               fill="none"
+//               stroke="currentColor"
+//               stroke-width="2"
+//               stroke-linecap="round"
+//               stroke-linejoin="round"
+//               className="w-4 h-4"
+//             >
+//               <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
+//               <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
+//             </svg>
+//             <span>Facilities</span>
+//           </a>
+//           <a
+//             rel="noopener noreferrer"
+//             href="#"
+//             className="flex items-center flex-shrink-0 px-5 py-3 space-x-2  text-gray-600"
+//           >
+//             <svg
+//               xmlns="http://www.w3.org/2000/svg"
+//               viewBox="0 0 24 24"
+//               fill="none"
+//               stroke="currentColor"
+//               stroke-width="2"
+//               stroke-linecap="round"
+//               stroke-linejoin="round"
+//               className="w-4 h-4"
+//             >
+//               <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
+//             </svg>
+//             <span>Booking</span>
+//           </a>
+//           <a
+//             rel="noopener noreferrer"
+//             href="#"
+//             className="flex items-center flex-shrink-0 px-5 py-3 space-x-2  text-gray-600"
+//           >
+//             <svg
+//               xmlns="http://www.w3.org/2000/svg"
+//               viewBox="0 0 24 24"
+//               fill="none"
+//               stroke="currentColor"
+//               stroke-width="2"
+//               stroke-linecap="round"
+//               stroke-linejoin="round"
+//               className="w-4 h-4"
+//             >
+//               <circle cx="12" cy="12" r="10"></circle>
+//               <polygon points="16.24 7.76 14.12 14.12 7.76 16.24 9.88 9.88 16.24 7.76"></polygon>
+//             </svg>
+//             <a>Event</a>
+//           </a>
+//           <a
+//             rel="noopener noreferrer"
+//             href="#"
+//             className="flex items-center flex-shrink-0 px-5 py-3 space-x-2  text-gray-600"
+//           >
+//             <svg
+//               xmlns="http://www.w3.org/2000/svg"
+//               viewBox="0 0 24 24"
+//               fill="none"
+//               stroke="currentColor"
+//               stroke-width="2"
+//               stroke-linecap="round"
+//               stroke-linejoin="round"
+//               className="w-4 h-4"
+//             >
+//               <circle cx="12" cy="12" r="10"></circle>
+//               <polygon points="16.24 7.76 14.12 14.12 7.76 16.24 9.88 9.88 16.24 7.76"></polygon>
+//             </svg>
+//             <span>Chat Forum</span>
+//           </a>
+//           <a
+//             rel="noopener noreferrer"
+//             href="#"
+//             className="flex items-center flex-shrink-0 px-5 py-3 space-x-2  text-gray-600"
+//           >
+//             <svg
+//               xmlns="http://www.w3.org/2000/svg"
+//               viewBox="0 0 24 24"
+//               fill="none"
+//               stroke="currentColor"
+//               stroke-width="2"
+//               stroke-linecap="round"
+//               stroke-linejoin="round"
+//               className="w-4 h-4"
+//             >
+//               <circle cx="12" cy="12" r="10"></circle>
+//               <polygon points="16.24 7.76 14.12 14.12 7.76 16.24 9.88 9.88 16.24 7.76"></polygon>
+//             </svg>
+//             <span>Map</span>
+//           </a>
+//         </div>
+//       </body>
+//       <div className="flex items-center justify-center h-screen mx-auto">
+//   <Map
+//     google={google}
+//     initialCenter={{ lat: 43.653208, lng: -79.463415 }}
+//     ref={mapRef}
+    
+//   >
+//     {parks.map((park) => (
+//       <Marker
+//         key={park.id}
+//         position={{ lat: park.lat, lng: park.lng }}
+//         title={park.name}
+//       />
+//     ))}
+//   </Map>
+// </div>
+
+//     </div>
+//   );
+// };
+// export default GoogleApiWrapper({
+//   apiKey: "AIzaSyCJBPJpIw3o9Oq2MOKz4JtudgaHnSwuIQs",
+// })(ParkMap);
